a7afebeb7dd76477755d688380ccfd36b721d644
Евгений Субочев <panc96@yandex.ru>
2023-07-16 22:57:38 +0300

    qwe

--- backend/cats/views.py
+++ backend/cats/views.py
@@ -1,5 +1,6 @@
 from rest_framework import viewsets
 from rest_framework.pagination import PageNumberPagination
+from rest_framework.response import Response
 
 from .models import Achievement, Cat
 from .serializers import AchievementSerializer, CatSerializer
@@ -12,6 +13,14 @@ class CatViewSet(viewsets.ModelViewSet):
 
     def perform_create(self, serializer):
         serializer.save(owner=self.request.user)
+
+    def retrieve(self, request, *args, **kwargs):
+        instance = self.get_object()
+        serializer = self.get_serializer(instance)
+        serializer.data['image'] = serializer.data['image'].replace(
+            'http://127.0.0.1:9000/', 'https://kitygramycprac.ddns.net/'
+        )
+        return Response(serializer.data)
 
 
 class AchievementViewSet(viewsets.ModelViewSet):
