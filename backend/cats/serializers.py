5fa368c1fc2407f6cd35a1925a630c08933afeda
Евгений Субочев <panc96@yandex.ru>
2023-07-17 16:10:18 +0300

    review

--- backend/cats/serializers.py
+++ backend/cats/serializers.py
@@ -91,6 +91,5 @@ class CatSerializer(serializers.ModelSerializer):
             )
             lst.append(current_achievement)
         instance.achievements.set(lst)
-        # asd
         instance.save()
         return instance
